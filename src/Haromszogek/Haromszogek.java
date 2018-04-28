84064c7e25e142db8c151e6ae7d90795050230ad
fabiant0822 <>
2018-04-28 11:33:44 +0200

    haromszog 1.1

---
+++ src/Haromszogek/Haromszogek.java
@@ -0,0 +1,150 @@
+package Haromszogek;
+
+import java.util.ArrayList;
+import javax.swing.DefaultListModel;
+
+/**
+ *
+ * @author Fabian Tamas 1.0
+ */
+public class Haromszogek extends javax.swing.JFrame {
+
+    DefaultListModel<String> lm = new DefaultListModel<>();
+    ArrayList<DHaromszog> haromszogek = new ArrayList<>();
+  
+    public Haromszogek() {
+        initComponents();
+        lstHaromszogek.setModel(lm);
+    }
+    
+    
+
+    
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        fcMegnyit = new javax.swing.JFileChooser();
+        btnBetolt = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        txaHibak = new javax.swing.JTextArea();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        lstHaromszogek = new javax.swing.JList<>();
+        jScrollPane3 = new javax.swing.JScrollPane();
+        txaAdatok = new javax.swing.JTextArea();
+
+        fcMegnyit.setApproveButtonMnemonic(77);
+        fcMegnyit.setApproveButtonText("Megnyitás");
+        fcMegnyit.setApproveButtonToolTipText("");
+        fcMegnyit.setControlButtonsAreShown(false);
+        fcMegnyit.setCurrentDirectory(new File("."));
+        fcMegnyit.setDialogTitle("Megnyitás");
+        fcMegnyit.setToolTipText("");
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Derékszögű háromszögek");
+
+        btnBetolt.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        btnBetolt.setText("Adatok betöltése");
+
+        txaHibak.setEditable(false);
+        txaHibak.setColumns(20);
+        txaHibak.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        txaHibak.setRows(5);
+        txaHibak.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "Hibák a kiválasztott állományban", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Tahoma", 0, 14))); // NOI18N
+        jScrollPane1.setViewportView(txaHibak);
+
+        lstHaromszogek.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "Derékszögű háromszögek", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Tahoma", 0, 14))); // NOI18N
+        lstHaromszogek.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        lstHaromszogek.setSelectionMode(javax.swing.ListSelectionModel.SINGLE_SELECTION);
+        lstHaromszogek.setToolTipText("");
+        jScrollPane2.setViewportView(lstHaromszogek);
+
+        txaAdatok.setEditable(false);
+        txaAdatok.setColumns(20);
+        txaAdatok.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        txaAdatok.setRows(5);
+        txaAdatok.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "Kiválasztott derékszögű háromszög adatai", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Tahoma", 0, 14))); // NOI18N
+        jScrollPane3.setViewportView(txaAdatok);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane1)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 286, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jScrollPane3, javax.swing.GroupLayout.DEFAULT_SIZE, 423, Short.MAX_VALUE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(btnBetolt, javax.swing.GroupLayout.PREFERRED_SIZE, 281, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(0, 0, Short.MAX_VALUE)))
+                .addContainerGap())
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(btnBetolt, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 151, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 275, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 165, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        pack();
+        setLocationRelativeTo(null);
+    }// </editor-fold>//GEN-END:initComponents
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Haromszogek.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Haromszogek.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Haromszogek.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Haromszogek.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Haromszogek().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnBetolt;
+    private javax.swing.JFileChooser fcMegnyit;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JScrollPane jScrollPane3;
+    private javax.swing.JList<String> lstHaromszogek;
+    private javax.swing.JTextArea txaAdatok;
+    private javax.swing.JTextArea txaHibak;
+    // End of variables declaration//GEN-END:variables
+}
